5890e7013f48c3b235aa376fca0f5a5d96104a40
Rishi <rishiicreates@gmail.com>
2025-03-08 19:26:31 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Stong Will Rule, Weak Will Survive.
+        </p>
+
+        <AnimatedTitle
+          title=" the <b>G</b>reatest story <br /> ever <b>T</b>old !!"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Unwavering will adventure, dreams and to chase the impossible.</p>
+          <p className="text-gray-500">
+            With a wide grin and an unshakable resolve, he sets sail into the unknown, not for riches or power, but for the ultimate freedom—to explore, to live, and to carve his name into history.
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
